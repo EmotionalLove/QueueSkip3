a51cc63519f281419c3f868f88607ccbeca37e16
Sasha <sashwho@vivaldi.net>
2018-10-27 23:43:32 -0700

    gradle boi and main class

---
+++ src/main/java/com/sasha/queueskip/Main.java
@@ -0,0 +1,31 @@
+package com.sasha.queueskip;
+
+import com.sasha.reminecraft.api.RePlugin;
+
+public class Main extends RePlugin {
+
+    @Override
+    public void onPluginInit() {
+
+    }
+
+    @Override
+    public void onPluginEnable() {
+
+    }
+
+    @Override
+    public void onPluginDisable() {
+
+    }
+
+    @Override
+    public void registerCommands() {
+
+    }
+
+    @Override
+    public void registerConfig() {
+
+    }
+}
