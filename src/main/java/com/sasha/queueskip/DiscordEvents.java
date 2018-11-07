16eab900f5bbb4ab4ccfaea4e84901c2390e6486
Sasha <sashwho@vivaldi.net>
2018-11-06 18:44:40 -0800

    gay

--- src/main/java/com/sasha/queueskip/DiscordEvents.java
+++ src/main/java/com/sasha/queueskip/DiscordEvents.java
@@ -10,12 +10,14 @@ public class DiscordEvents extends ListenerAdapter {
     public void onDMRx(PrivateMessageReceivedEvent e) {
         if (e.getAuthor().getId().equals(Main.CONFIG.var_managerId)) {
             DiscordUtils.recievedMessage = e.getMessage();
-            e.getMessage().getChannel().sendTyping().queue();
-            if (e.getMessage().getContentDisplay().startsWith(";")) {
-                Main.COMMAND_PROCESSOR.processCommand(e.getMessage().getContentDisplay());
-                return;
-            }
-            e.getMessage().getChannel().sendMessage(DiscordUtils.buildErrorEmbed("Commands must start with \";\"")).queue();
+            e.getMessage().getChannel().sendTyping().queue(type -> {
+                if (e.getMessage().getContentDisplay().startsWith(";")) {
+                    Main.COMMAND_PROCESSOR.processCommand(e.getMessage().getContentDisplay());
+                    return;
+                }
+                e.getMessage().getChannel().sendMessage(DiscordUtils.buildErrorEmbed("Commands must start with \";\"")).queue();
+            });
+
         }
     }
 }
