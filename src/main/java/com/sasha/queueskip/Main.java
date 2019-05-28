2077d356523aa72033a213d4f2dffedd1d517909
EmotionalLove <sashwho@vivaldi.net>
2019-05-28 01:17:01 -0700

    increment version

--- src/main/java/com/sasha/queueskip/Main.java
+++ src/main/java/com/sasha/queueskip/Main.java
@@ -19,7 +19,7 @@ import javax.security.auth.login.LoginException;
 public class Main extends RePlugin {
 
     public static Main INSTANCE;
-    public static final String VERSION = "3.2.1";
+    public static final String VERSION = "3.2.2";
 
     public static JDA discord;
 
