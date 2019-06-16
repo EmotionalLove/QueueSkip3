51ca4f88c70c378147fa98d912732306cec2d708
EmotionalLove <sashwho@vivaldi.net>
2019-06-15 21:39:43 -0700

    add sum spammy bois

--- src/main/java/com/sasha/queueskip/QSkipConfig.java
+++ src/main/java/com/sasha/queueskip/QSkipConfig.java
@@ -2,6 +2,8 @@ package com.sasha.queueskip;
 
 import com.sasha.reminecraft.Configuration;
 
+import java.util.ArrayList;
+
 public class QSkipConfig extends Configuration {
 
     @ConfigSetting
@@ -26,6 +28,17 @@ public class QSkipConfig extends Configuration {
     public boolean var_debug = false;
     @ConfigSetting
     public boolean var_connectingAlert = true;
+    @ConfigSetting
+    public ArrayList<String> var_spamMessages = new ArrayList<>();
+
+    {
+        var_spamMessages.add("> QueueSkip Discord - https://discord.gg/YKRY6P3 <");
+        var_spamMessages.add("> 2b2t style anarchy, without the queue - nemuii.org <");
+        var_spamMessages.add("> This account is connected with QueueSkip - https://discord.gg/YKRY6P3 <");
+        var_spamMessages.add("> Don't pay $20 for the priority, pay $5 for the skip - https://discord.gg/YKRY6P3 <");
+        var_spamMessages.add("> No queue, fresh world, elytra enabled - nemuii.org <");
+        var_spamMessages.add("> Meet a rare female 2b2t player - https://discord.gg/YKRY6P3 <");
+    }
 
     QSkipConfig() {
         super("QueueSkip3");
