e7d657c6bf70d3c43cc8ce6bb50a338ba3bf5a6d
EmotionalLove <please@someonelove.me>
2019-12-31 03:44:18 -0800

    inc version

--- src/main/java/com/sasha/queueskip/Main.java
+++ src/main/java/com/sasha/queueskip/Main.java
@@ -30,7 +30,7 @@ import static com.sasha.queueskip.Util.isConnected;
 public class Main extends RePlugin {
 
     public static Main INSTANCE;
-    public static final String VERSION = "3.2.5";
+    public static final String VERSION = "3.3";
 
     public static JDA discord;
 
